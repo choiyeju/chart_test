784e2d33f1347eb18e28f79728c712bfe02a20a5
최예주 <yeju.choi@trialinformatics.com>
2024-02-20 13:48:15 +0900

    feat: chart-otb 페이지 추가

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ChartTest = lazy(() => import('views/chart-test'));
+const ChartOtb = lazy(() => import('views/chart-otb'));
 
 const routes = [
     {
@@ -11,6 +12,10 @@ const routes = [
     {
         path: "/chart-test",
         element: <ChartTest/>,
+    },
+    {
+        path: "/chart-otb",
+        element: <ChartOtb/>,
     },
 ];
 
