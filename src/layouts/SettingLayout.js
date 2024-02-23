a5b8178f38c9e6ab492dbf6d87150d669dbbdec1
최예주 <yeju.choi@trialinformatics.com>
2024-02-23 14:08:53 +0900

    feat: chart-setting 페이지 추가

--- src/layouts/SettingLayout.js
+++ src/layouts/SettingLayout.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useRecoilState } from "recoil";
+import {blineValuesState, chartTypeState} from "stores";
 
 export const SettingLayout = ({children}) => {
+    const [settingType, setSettingType] = useState('axes');
+    const [chartType, setChartType] = useRecoilState(chartTypeState);
+
+    const [blineValues, setBLineValues] = useRecoilState(blineValuesState);
+    const [values, setValues] = useState(null);
+
+    const handleChangeText = (e, axis) => {
+        const text = JSON.parse(JSON.stringify(values.text));
+        text[axis] = e.target.value;
+        setValues({ ...values, text });
+    };
+
+    useEffect(() => {
+        if (chartType === 'bline') {
+            if (values) setBLineValues(values);
+            else setValues(blineValues);
+            // console.log(values);
+        } else if (!values && chartType === 'line') {
+
+        } else if (!values && chartType === 'bar') {
+
+        }
+    }, [values]);
+
+    if (!values) return <>로딩중</>;
+
     return (
-        <>
+        <div className="setting">
+            <div className="setting_sidebar">
+                {settingType === 'axes'?
+                    <>
+                        x: <input value={values.text.x} onChange={e => handleChangeText(e, "x")} /><br/>
+                        y: <input value={values.text.y} onChange={e => handleChangeText(e, "y")} /><br/>
+                    </> :
+                    settingType === 'color'?
+                    <>
+                        color
+                    </>:
+                settingType === "datalabels"?
+                    <>
+                        datalabels
+                    </>:
+                    <>
+                        tooltip
+                    </>
+                }
+            </div>
             {children}
-        </>
+        </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom';
 const ChartTest = lazy(() => import('views/chart-test'));
 const ChartOtb = lazy(() => import('views/chart-otb'));
 const ChartCustom = lazy(() => import('views/chart-custom'));
+const SettingLayout = lazy(() => import('views/chart-setting'));
 
 const routes = [
     {
@@ -21,6 +22,10 @@ const routes = [
     {
         path: "/chart-custom",
         element: <ChartCustom/>,
+    },
+    {
+        path: "/chart-setting",
+        element: <SettingLayout/>,
     },
 ];
 
